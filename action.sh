a13ab695ccf23d796897678c855b023f4a2ad058
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-01 12:16:51 -0700

    chore: add latest change on action.sh

--- action.sh
+++ action.sh
@@ -39,6 +39,6 @@ directory="."
 # iterate through the folders in the directory
 for foldername in "$directory"/*; do
     # rename the folder by replacing the hyphen with a space
-    new_name="$(basename "$foldername" | sed 's/-/ /g')"
+    new_name="$(basename "$foldername" | sed 's/ /-/g')"
     mv "$foldername" "$directory/$new_name"
 done
