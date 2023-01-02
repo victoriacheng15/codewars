82b57c584480ac1726ed1872327fff5c3618e245
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-02 11:09:27 -0700

    feat: merge add_readme to create_files

--- scripts/create_files.sh
+++ scripts/create_files.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 dir=$(pwd)
+folder=$(basename "$dir")
 
-folder="$(basename $dir)"
+title="$(echo "$folder" | sed "s/\b[a-z]/\U&/g" | sed "s/-/ /g")"
 
-echo $folder
+echo "# $title" > README.md
 
 # Check if at least one argument was provided
 if [ $# -eq 0 ]; then
