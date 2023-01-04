3108d814e019a4a1f4e9c783662c363d386e85b6
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-04 10:26:21 -0700

    feat: add py and ts solutions for cat dog years

---
+++ 8kyu/cat-years-dog-years/cat-years-dog-years.ts
@@ -0,0 +1,23 @@
+export function humanYearsCatYearsDogYears(
+	humanYears: number,
+): [number, number, number] {
+  let dogYears = 0;
+  let catYears = 0;
+  for (let i = 1; i <= humanYears; i += 1) {
+    switch (i) {
+      case 1:
+        catYears += 15;
+        dogYears += 15;
+        break;
+      case 2:
+        catYears += 9;
+        dogYears += 9;
+        break;
+      default:
+        catYears += 4;
+        dogYears += 5;
+    }
+  }
+  
+  return [humanYears, catYears, dogYears];
+}
