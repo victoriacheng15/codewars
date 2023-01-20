1926924375a118690fb078daec4165aec2658afb
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-20 09:07:55 -0700

    feat: add py create the room solution

---
+++ 8kyu/grasshopper-create-the-room/grasshopper-create-the-room.py
@@ -0,0 +1,18 @@
+# create 3 rooms with room1, room2 and room3, and each room should have name, description and completed status
+rooms = {
+  'room1': {
+    'name': 'room1',
+    'description': 'This is room1',
+    'completed': False
+  },
+  'room2': {
+    'name': 'room2',
+    'description': 'This is room2',
+    'completed': False
+  },
+  'room3': {
+    'name': 'room3',
+    'description': 'This is room3',
+    'completed': False
+  }
+}
