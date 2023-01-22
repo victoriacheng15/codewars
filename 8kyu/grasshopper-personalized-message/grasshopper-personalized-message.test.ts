b284712e939685120724640c386c6219beb41b14
Victoria Cheng <victoria.cheng15@gmail.com>
2023-01-22 12:50:53 -0700

    feat: add py ts personalized msg solutions

---
+++ 8kyu/grasshopper-personalized-message/grasshopper-personalized-message.test.ts
@@ -0,0 +1,16 @@
+import { describe, it, expect } from "vitest";
+
+export function greet(name: string, owner: string): string {
+	return name === owner ? "Hello boss" : "Hello guest";
+}
+
+describe("greet", () => {
+	// write unit tests that if name === owner return hello boss, otherwise return hello guest
+	it("should return hello boss", () => {
+		expect(greet("Daniel", "Daniel")).toBe("Hello boss");
+	});
+
+  it("should return hello guest", () => {
+    expect(greet("Greg", "Daniel")).toBe("Hello guest");
+  })
+});
