ba493915fea2ca673f5bc458e2ad7cf991753307
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-05 13:04:16 -0700

    chore: add py ts sum of diff array solutions

---
+++ 8kyu/sum-of-differences-in-array/sum-of-differences-in-array.ts
@@ -0,0 +1,9 @@
+export function sumOfDifferences(arr: number[]): number {
+  return arr.sort((a, b)  => b - a).reduce((acc, num, index) =>  {
+    const next = arr[index + 1];
+    const diff = num - next;
+    if(!isNaN(diff)) acc += diff
+
+    return acc
+  }, 0)
+}
