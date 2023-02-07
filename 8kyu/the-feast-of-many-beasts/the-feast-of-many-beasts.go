d785ab66ee3337a943459dc28ed2b845577ab3bc
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-07 09:13:02 -0700

    chore: add go py ts the feasts of the many beats solutions

---
+++ 8kyu/the-feast-of-many-beasts/the-feast-of-many-beasts.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+)
+
+func Feast(beast string, dish string) bool {
+  return beast[0] == dish[0] && beast[len(beast) - 1] == dish[len(dish) - 1] 
+}
+
+func main() {
+	fmt.Println(Feast("cat", "dog"))
+}
