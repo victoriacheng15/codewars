d539ab9d3ac2893d6ddb20904b873ce9dd47a33b
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-09 09:50:43 -0700

    chore: add go py ts volume of a cuboid solutions

---
+++ 8kyu/volume-of-a-cuboid/volume-of-a-cuboid.go
@@ -0,0 +1,5 @@
+package main
+
+func GetVolumeOfCuboid(length, width, height float64) float64 {
+	return length * width * height
+}
---
+++ 8kyu/volume-of-a-cuboid/volume-of-a-cuboid.ts
@@ -0,0 +1,3 @@
+export function getVolumeOfCuboid(length: number, width:number, height:number): number {
+  return length * width * height;
+}
