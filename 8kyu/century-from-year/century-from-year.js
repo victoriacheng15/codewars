db30770fc8b603f7342bbe2adc4a1c6b8599e6e5
Victoria Cheng <35031228+victoriacheng15@users.noreply.github.com>
2023-02-17 10:37:08 -0700

    Solutions 02 17 (#54)
    * chore: add go py sql ts century from year solutions
    
    * feat: add sql square root and log solution
    
    * feat: add sql basic mod solution

---
+++ 8kyu/century-from-year/century-from-year.js
@@ -0,0 +1,4 @@
+function century(year) {
+  const calcCentury = Math.floor(year / 100);
+  return year % 100 === 0 ? calcCentury : calcCentury + 1;
+}
---
+++ 8kyu/easy-sql-square-root-and-log/easy-sql-square-root-and-log.sql
@@ -0,0 +1,1 @@
+SELECT SQRT(number1) as root, LOG10(number2) as log FROM decimals;
