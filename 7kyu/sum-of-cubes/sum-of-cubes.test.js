876d53922d61ae71885651e1e92fbe01f28c4b10
Victoria Cheng <victoria.cheng15@gmail.com>
2023-02-27 09:40:15 -0700

    chore: add go py sum of cubes solutions

--- 7kyu/sum-of-cubes/sum-of-cubes.test.js
+++ 7kyu/sum-of-cubes/sum-of-cubes.test.js
@@ -1,13 +1,4 @@
-/* 
-Write a function that takes a positive integer n, sums all the cubed values from 1 to n, and returns that sum.
-
-Assume that the input n will always be a positive integer.
-
-Examples: (Input --> output)
-
-2 --> 9 (sum of the cubes of 1 and 2 is 1 + 8)
-3 --> 36 (sum of the cubes of 1, 2, and 3 is 1 + 8 + 27)
-*/
+import { describe, expect, it } from "vitest";
 
 function sumCubes(n) {
 	const arr = [...new Array(n + 1).keys()].slice(1);
