114ffa0b7078cac7f957e35641b2bc36f57e7a2c
victoriacheng15 <victoriacheng15@users.noreply.github.com>
2023-03-06 17:36:08 +0000

    style: the code has been formatted by rome!

--- 7kyu/count-the-digit/count-the-digit.test.ts
+++ 7kyu/count-the-digit/count-the-digit.test.ts
@@ -1,29 +1,29 @@
 import { describe, expect, it } from "vitest";
 
 export class G964 {
-  public static nbDig(n: number, d: number): number {
-    let [count, index] = [0, 0];
-    while (index <=n) {
-      const square = (index ** 2).toString();
-      for(const digit of square) {
-        if (Number(digit) === d) count++;
-      }
-      index++;
-    }
-    return count;
-  }
+	public static nbDig(n: number, d: number): number {
+		let [count, index] = [0, 0];
+		while (index <= n) {
+			const square = (index ** 2).toString();
+			for (const digit of square) {
+				if (Number(digit) === d) count++;
+			}
+			index++;
+		}
+		return count;
+	}
 }
 
 describe("count-the-digit", () => {
-  const tests = [
-    [5750, 0, 4700],
-    [11011, 2, 9481],
-    [12224, 8, 7733],
-  ]
+	const tests = [
+		[5750, 0, 4700],
+		[11011, 2, 9481],
+		[12224, 8, 7733],
+	];
 
-  for(const [n, d, expected] of tests) {
-    it(`n = ${n}, d = ${d}, expected = ${expected}`, () => {
-      expect(G964.nbDig(n, d)).toBe(expected);
-    });
-  }
-})
+	for (const [n, d, expected] of tests) {
+		it(`n = ${n}, d = ${d}, expected = ${expected}`, () => {
+			expect(G964.nbDig(n, d)).toBe(expected);
+		});
+	}
+});
